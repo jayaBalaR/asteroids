00e3bb793c5767cc8b7cf2b128c9d6c8d9843bed
JAYABALAMBIKA.R <21074002+jayaBalaR@users.noreply.github.com>
2022-02-10 19:51:31 +0530

    Update asteroid.py

--- asteroid.py
+++ asteroid.py
@@ -23,58 +23,47 @@ if __name__=="__main__":
 
     st.title("**Asteroid Diameter Prediction**")
 
-
-
-
     start = time.time()
 
-
     uploaded_file = st.file_uploader("Choose a CSV file")
+    
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file.name)
 
-    df = pd.read_csv(uploaded_file.name)
-    # st.write(os.getcwd())
-    st.write("original dataframe")
-    st.dataframe(df)
+        st.write("original dataframe")
+        st.dataframe(df)
 
-    df.drop(['pha','class','condition_code','spec_B', 'spec_T','per.y','rot_per', 'BV', 'UB','G','data_arc', 'diameter' ],axis=1,inplace=True)
+        df.drop(['pha','class','condition_code','spec_B', 'spec_T','per.y','rot_per', 'BV', 'UB','G','data_arc', 'diameter' ],axis=1,inplace=True)
 
-    st.write("Dropping few columns")
-
-    # st.dataframe(df)
-
-
-    df_predict = df.copy(deep=True)
-
-    index_list = df_predict.index[df_predict['albedo'].isnull()].tolist()
-
-    np.random.seed(0)
-    mu = 0.14
-    sigma = float(0.01)
+        st.write("Dropping few columns")
 
 
-    #    print(norm_val)
+        df_predict = df.copy(deep=True)
 
-    for idx in tqdm(index_list):
-         df_predict.loc[idx,'albedo']= np.random.normal(mu, sigma ,size=1)
-    alblist = df_predict['albedo'].tolist() ###list of created albedo values
+        index_list = df_predict.index[df_predict['albedo'].isnull()].tolist()
+
+        np.random.seed(0)
+        mu = 0.14
+        sigma = float(0.01)
 
 
-    st.write("after inflecting albedo")
-    st.dataframe(df_predict)
+        for idx in tqdm(index_list):
+            df_predict.loc[idx,'albedo']= np.random.normal(mu, sigma ,size=1)
+        alblist = df_predict['albedo'].tolist() ###list of created albedo values
+
+
+        with st.spinner('Computing predictions'):
+            time.sleep(5)
+        st.success('Done!')
+        
 
     
-    test_predictions = new_model.predict(df_predict).flatten()
+        test_predictions = new_model.predict(df_predict).flatten()
 
-    df_predict['predicted_dia'] = test_predictions.tolist()
+        df_predict['predicted_dia'] = test_predictions.tolist()
 
-    st.dataframe(df_predict)
+        st.dataframe(df_predict)
 
 
-    # results=search(query)
-    # if results is not None:
-    #     datafr = pd.DataFrame({'results':results})
-    #
-    #     st.table(datafr)
-    #
-    # end = time.time()
+
     print(start)
