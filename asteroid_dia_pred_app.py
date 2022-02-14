1f04e43bd039b97902ee43019c6620db628b1597
JAYABALAMBIKA.R <21074002+jayaBalaR@users.noreply.github.com>
2022-02-14 22:24:04 +0530

    Update asteroid_dia_pred_app.py

--- asteroid_dia_pred_app.py
+++ asteroid_dia_pred_app.py
@@ -25,52 +25,51 @@ if __name__=="__main__":
 
     start = time.time()
     
-    number = st.number_input('Insert a number', min_value=0.0, max_value=10.1, step=0.1)
-    st.write('The current number is ', number)
-
-#     magnitude = st.number_input('magnitude', 3.4, 29.0, 0.1)
-#     st.write("The H value= ", magnitude, 'au')
+    magnitude = st.number_input('magnitude', min_value=3.4, max_value=29.0, step=0.1)
+    st.write("The H value= ", magnitude, 'au')
     
-#     albedo = st.number_input('albedo', 0.00, 98.00, 0.01)
-#     st.write("The geometric albedo(ratio) value= ", albedo)
+    albedo = st.number_input('albedo', min_value=0.00, max_value=98.00, step=0.01)
+    st.write("The geometric albedo(ratio) value= ", albedo)
     
-#     e = st.number_input('eccentricity', 0.0, 0.99, 0.1)
-#     st.write("The eccentricity(ratio) value= ", e)
+    e = st.number_input('eccentricity', min_value=0.0, max_value=0.99, step=0.1)
+    st.write("The eccentricity(ratio) value= ", e)
     
-#     a = st.number_input('semimajor axis', 0.6, 385.0, 0.1)
-#     st.write("The semimajor axis value= ", a, 'au')
+    a = st.number_input('semimajor axis', min_value=0.6, max_value=385.0, step=0.1)
+    st.write("The semimajor axis value= ", a, 'au')
     
-#     q = st.number_input('perihelion distance', 0.08, 40.0, 0.1)
-#     st.write("The perihelion distance value= ", q, 'au')
+    q = st.number_input('perihelion distance', min_value=0.08, max_value=40.0, step=0.1)
+    st.write("The perihelion distance value= ", q, 'au')
     
-#     i = st.number_input('inclination', 0.0, 180.0, 10.0)
-#     st.write("The inclination value= ", i)
+    i = st.number_input('inclination', min_value=0.0, max_value=180.0, step=10.0)
+    st.write("The inclination value= ", i)
     
-#     om = st.number_input('longitude of ascending node', 0.0, 360.0, 10.0)
-#     st.write("The longitude of ascending node= ", om)
+    om = st.number_input('longitude of ascending node', min_value=0.0, max_value=360.0, step=10.0)
+    st.write("The longitude of ascending node= ", om)
     
-#     #'w', 'ma','ad', 'n', 'per', 'moid'
-#     w = st.number_input('argument of perihelion', 0.0, 360.0, 10.0)
-#     st.write("The peri= ", w)
+    #'w', 'ma','ad', 'n', 'per', 'moid'
+    w = st.number_input('argument of perihelion', min_value=0.0, max_value=360.0, step=10.0)
+    st.write("The peri= ", w)
     
-#     ma = st.number_input('mean anomaly', 0.0, 367.0, 1.0)
-#     st.write("The mean anomaly= ", ma)
+    ma = st.number_input('mean anomaly', min_value=0.0, max_value=367.0, step=1.0)
+    st.write("The mean anomaly= ", ma)
     
-#     ad = st.number_input('aphelion distance', 1.0, 764.0, 1.0)
-#     st.write("The aphelion distance= ", ad)
+    ad = st.number_input('aphelion distance', min_value=1.0, max_value=764.0, step=1.0)
+    st.write("The aphelion distance= ", ad)
     
-#     n = st.number_input('mean motion', 0.0, 1.0, 1.0)
-#     st.write("The mean motion= ", n)
+    n = st.number_input('mean motion', min_value=0.0, max_value=1.0, step=0.1)
+    st.write("The mean motion= ", n)
         
 
-#     per = st.number_input('orbital period', 181.0, 2.760000e+06, 1.0, 1.0 )
-#     st.write("The orbital period= ", per)
+    per = st.number_input('orbital period', min_value=181.0, max_value=2.760000e+06, step=1.0 )
+    st.write("The orbital period= ", per)
     
     
-#     moid = st.number_input('earth min orbit dist', 0.0, 39.0, 1.0)
-#     st.write("earth min orbit dist= ", moid, 'au')
+    moid = st.number_input('earth min orbit dist', min_value=0.0, max_value=39.0, step=1.0)
+    st.write("earth min orbit dist= ", moid, 'au')
 
-#     scaler = StandardScaler()
+    scaler = StandardScaler()
+    inp_array = [[magnitude,albedo,e,a,q,i,om,w,ma,ad,n,per,moid]]
+    st.write(inp_array)
 
     
 
