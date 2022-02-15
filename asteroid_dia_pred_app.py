cd0445c0b4a97647e8fab9dee1cf81011758b728
JAYABALAMBIKA.R <21074002+jayaBalaR@users.noreply.github.com>
2022-02-15 15:44:47 +0530

    Update asteroid_dia_pred_app.py

--- asteroid_dia_pred_app.py
+++ asteroid_dia_pred_app.py
@@ -77,9 +77,11 @@ if __name__=="__main__":
 
         
         inp_array = [[magnitude,albedo,e,a,q,i,om,w,ma,ad,n,per,moid]]
+        st.write(inp_array)
         inp_features = scaler.fit_transform(inp_array)
-    
+        st.write(inp_features)
         st.spinner(text="Computing model predictions")
         model_pred = my_model.predict(inp_features).flatten()
+        
         st.write("diameter")
         st.table(model_pred)
