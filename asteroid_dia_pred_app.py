ced20b520a0b4f37d3ec62c73c7bf5751153ccf4
JAYABALAMBIKA.R <21074002+jayaBalaR@users.noreply.github.com>
2022-02-18 16:17:19 +0530

    Update asteroid_dia_pred_app.py

--- asteroid_dia_pred_app.py
+++ asteroid_dia_pred_app.py
@@ -104,5 +104,5 @@ if __name__=="__main__":
                 else:
                     st.markdown("**This is  a PHA**")
        
-       st.write('total time elapsed since the start is', str(time_elapsed))
+        st.write('total time elapsed since the start is', str(time_elapsed))
         
