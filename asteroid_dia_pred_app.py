64cee26046c7abcd1a5c84d430c5315e37e7b0cd
JAYABALAMBIKA.R <21074002+jayaBalaR@users.noreply.github.com>
2022-02-19 15:15:15 +0530

    Update asteroid_dia_pred_app.py

--- asteroid_dia_pred_app.py
+++ asteroid_dia_pred_app.py
@@ -44,58 +44,58 @@ if __name__=="__main__":
         
         with col1:
             magnitude = st.number_input('magnitude', min_value=3.4, max_value=29.0, key="H")
-            st.write("The H value= ", magnitude, 'au')
+            st.write("The H value= ", st.session_state.H, 'au')
     
             albedo = st.number_input('albedo', min_value=0.00, max_value=98.00, key="albedo")
-            st.write("The geometric albedo(ratio) value= ", albedo)
+            st.write("The geometric albedo(ratio) value= ", st.session_state.albedo)
     
             e = st.number_input('eccentricity', min_value=0.0, max_value=0.99, key="e")
-            st.write("The eccentricity(ratio) value= ", e)
+            st.write("The eccentricity(ratio) value= ", st.session_state.e)
     
             a = st.number_input('semimajor axis', min_value=0.6, max_value=385.0, key="a")
-            st.write("The semimajor axis value= ", a, 'au')
+            st.write("The semimajor axis value= ", st.session_state.a, 'au')
             
-            st.session_state.albedo
-            st.session_state.H
-            st.session_state.e
-            st.session_state.a
+#             st.session_state.albedo
+#             st.session_state.H
+#             st.session_state.e
+#             st.session_state.a
             
         
         with col2:
-            q = st.number_input('perihelion distance', min_value=0.08, max_value=40.0)
-            st.write("The perihelion distance value= ", q, 'au')
+            q = st.number_input('perihelion distance', min_value=0.08, max_value=40.0, key="q")
+            st.write("The perihelion distance value= ", st.session_state.q, 'au')
     
-            i = st.number_input('inclination', min_value=0.0, max_value=180.0)
-            st.write("The inclination value= ", i, 'degrees')
+            i = st.number_input('inclination', min_value=0.0, max_value=180.0, key="i")
+            st.write("The inclination value= ", st.session_state.i, 'degrees')
     
-            om = st.number_input('longitude of ascending node', min_value=0.0, max_value=360.0)
-            st.write("The longitude of ascending node= ", om, 'degrees')
+            om = st.number_input('longitude of ascending node', min_value=0.0, max_value=360.0, key="om")
+            st.write("The longitude of ascending node= ", st.session_state.om, 'degrees')
     
             #'w', 'ma','ad', 'n', 'per', 'moid'
-            w = st.number_input('argument of perihelion', min_value=0.0, max_value=360.0)
-            st.write("The peri= ", w, 'degrees')
+            w = st.number_input('argument of perihelion', min_value=0.0, max_value=360.0, key="w")
+            st.write("The peri= ", st.session_state.w, 'degrees')
     
-            ma = st.number_input('mean anomaly', min_value=0.0, max_value=367.0)
-            st.write("The mean anomaly= ", ma, 'degrees')
+            ma = st.number_input('mean anomaly', min_value=0.0, max_value=367.0, key="ma")
+            st.write("The mean anomaly= ", st.session_state.ma, 'degrees')
        
         with col3:
         
-            ad = st.number_input('aphelion distance', min_value=1.0, max_value=764.0)
-            st.write("The aphelion distance= ", ad, 'au')
+            ad = st.number_input('aphelion distance', min_value=1.0, max_value=764.0, key="ad")
+            st.write("The aphelion distance= ", st.session_state.ad, 'au')
     
-            n = st.number_input('mean motion', min_value=0.0, max_value=1.0)
-            st.write("The mean motion= ", n, 'deg/d')
+            n = st.number_input('mean motion', min_value=0.0, max_value=1.0, key="n")
+            st.write("The mean motion= ", st.session_state.n, 'deg/d')
         
 
-            per = st.number_input('orbital period', min_value=181.0, max_value=2.760000e+06)
-            st.write("The orbital period= ", per, 'days')
+            per = st.number_input('orbital period', min_value=181.0, max_value=2.760000e+06, key="per")
+            st.write("The orbital period= ", st.session_state.per, 'days')
     
     
-            moid = st.number_input('earth min orbit dist', min_value=0.0, max_value=39.0)
-            st.write("earth min orbit dist= ", moid, 'au')
+            moid = st.number_input('earth min orbit dist', min_value=0.0, max_value=39.0, key="moid")
+            st.write("earth min orbit dist= ", st.session_state.moid, 'au')
             
         with col4:        
-            inp_array = [[magnitude,albedo,e,a,q,i,om,w,ma,ad,n,per,moid]]
+            inp_array = [[st.session_state.H,st.session_state.albedo,st.session_state.e,st.session_state.a,st.session_state.q,st.session_state.i,st.session_state.om,st.session_state.w,st.session_state.ma,st.session_state.ad,st.session_state.n,st.session_state.per,st.session_state.moid]]
             
             submitted1 = st.form_submit_button('Submit 1')
 
